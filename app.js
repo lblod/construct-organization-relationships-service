18786ca6f5bf2f9e6a7899a87662aeab5c072244
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2023-04-18 18:47:45 +0200

    disable agb & apb (#5)

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import { app, errorHandler } from 'mu';
 import { createAdministrativeUnitdRelationships } from './lib/processing-administrative-unit'
 
-app.get('/', function( req, res ) {
+app.get('/', function(req, res) {
   res.send('Hello from construct-administrative-unit-relationships-service !');
-} );
+});
 
-app.post('/create-relationships/:administrativeUnitdUuid', async function( req, res ) {
+app.post('/create-relationships/:administrativeUnitdUuid', async function(req, res) {
   try {
     const administrativeUnitdUuid = req.params.administrativeUnitdUuid;
     await createAdministrativeUnitdRelationships(administrativeUnitdUuid);
-    return res.status(204).send();
+    return res.status(200).send(); // since we await, it should be 200
   } catch (e) {
     console.log("Something went wrong while calling /create-relationship", e);
     return res.status(500).send();
   }
-} );
+});
 
 app.use(errorHandler);
