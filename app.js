bac52109eba554e26b3614f40b36e9ec192489e4
Damon <damonkennes@gmail.com>
2026-06-29 16:01:07 +0200

    OP-3810: draft version inoprichting life cycle

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { app, errorHandler } from "mu";
 import { createOrganizationRelationships } from "./lib/processing-organization";
+import { handleStatusChange } from "./lib/status-transitions";
 
 app.get("/", function (req, res) {
   res.send("Hello from construct-organization-relationships-service !");
@@ -12,6 +13,17 @@ app.post("/create-relationships/:organizationUuid", async function (req, res) {
     return res.status(200).send(); // since we await, it should be 200
   } catch (e) {
     console.log("Something went wrong while calling /create-relationship", e);
+    return res.status(500).send();
+  }
+});
+
+app.post("/update-relationships/:organizationUuid", async function (req, res) {
+  try {
+    const organizationUuid = req.params.organizationUuid;
+    await handleStatusChange(organizationUuid);
+    return res.status(200).send();
+  } catch (e) {
+    console.log("Something went wrong while calling /update-relationships", e);
     return res.status(500).send();
   }
 });
